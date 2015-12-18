92ea3bd101fc3c152b868112a9f2ba5230dae838
Jacks Who <191100479@qq.com>
2015-12-18 20:47:23 +0800

    [add]:add QR code for QQ and wechat

--- blog/js/init.js
+++ blog/js/init.js
@@ -5,44 +5,84 @@
 */
 
 // Skel.
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			'global': { range: '*', href: 'blog/css/style.css', lockViewport: true, viewport: 'minimal-ui' },
-			'wide': { range: '-1680', href: 'blog/css/style-wide.css' },
-			'normal': { range: '-1280', href: 'blog/css/style-normal.css' },
-			'mobile': { range: '-640', href: 'blog/css/style-mobile.css' },
-			'mobilep': { range: '-360', href: 'blog/css/style-mobilep.css' }
-		}
-	});
+skel.init({
+  reset: 'full',
+  breakpoints: {
+    'global': {
+      range: '*',
+      href: 'blog/css/style.css',
+      lockViewport: true,
+      viewport: 'minimal-ui'
+    },
+    'wide': {
+      range: '-1680',
+      href: 'blog/css/style-wide.css'
+    },
+    'normal': {
+      range: '-1280',
+      href: 'blog/css/style-normal.css'
+    },
+    'mobile': {
+      range: '-640',
+      href: 'blog/css/style-mobile.css'
+    },
+    'mobilep': {
+      range: '-360',
+      href: 'blog/css/style-mobilep.css'
+    },
+    'qr': {
+      range: '*',
+      href: 'blog/css/QR-code.css'
+    }
+  }
+});
 
 // Events (JS).
 
-	// Remove "loading" class once the page has fully loaded.
-		window.onload = function() {
-			document.body.className = '';
-			var sum=Math.floor(Math.random()*4);
-			var images_bg_url="blog/css/images/bg.jpg"
-			var images_bg_url1="blog/css/images/bg1.jpg"
-			var images_bg_url2="blog/css/images/bg2.jpg"
-			var images_bg_url3="blog/css/images/bg3.jpg"
-			var images=[];
-			images.push(images_bg_url,images_bg_url1,images_bg_url2,images_bg_url3);
-			switch (sum) {
-				case 0:document.getElementById("bg").style.backgroundImage="url("+images[sum]+")";break;
-				default:document.getElementById("bg").style.background="url("+images[sum]+")";break;
-			}
-		}
+// Remove "loading" class once the page has fully loaded.
+window.onload = function() {
+    document.body.className = '';
+    var sum = Math.floor(Math.random() * 4);
+    var images_bg_url = "blog/css/images/bg.jpg"
+    var images_bg_url1 = "blog/css/images/bg1.jpg"
+    var images_bg_url2 = "blog/css/images/bg2.jpg"
+    var images_bg_url3 = "blog/css/images/bg3.jpg"
+    var images = [];
+    images.push(images_bg_url, images_bg_url1, images_bg_url2, images_bg_url3);
+    switch (sum) {
+      case 0:
+        document.getElementById("bg").style.backgroundImage = "url(" + images[sum] + ")";
+        break;
+      default:
+        document.getElementById("bg").style.background = "url(" + images[sum] + ")";
+        break;
+    }
+    if (!window.IsPC()) {
+      document.getElementById("qq_pc").href = '#qq';
+    }
+  }
+  // IsPC
+window.IsPC = function() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+      if (userAgentInfo.indexOf(Agents[v]) > 0) {
+        flag = false;
+        break;
+      }
+    }
+    return flag;
+  }
+  // Prevent scrolling on touch.
+window.ontouchmove = function() {
+  return false;
+}
 
-	// Prevent scrolling on touch.
-		window.ontouchmove = function() {
-			return false;
-		}
-
-	// Fix scroll position on orientation change.
-		window.onorientationchange = function() {
-			document.body.scrollTop = 0;
-		}
+// Fix scroll position on orientation change.
+window.onorientationchange = function() {
+  document.body.scrollTop = 0;
+}
 
 /*
 
